70ede6ea9b8a22b6fbe4558b0f58533dc859254c
Gurvindar singh <132663008+Gscoder27@users.noreply.github.com>
2025-03-16 23:39:03 +0530

    Update json-server to stable version and fix start script

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
+const cors = require('cors');
+server.use(cors());
+
 server.use(middlewares);
 server.use(router);
+
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
